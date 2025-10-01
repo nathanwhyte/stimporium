1523ea70b0950f7a968270d460c623793b29c942
Nathan Whyte <nathanwhyte35@gmail.com>
2025-10-01 15:18:27 -0500

    Add tool for formatting embedded tailwind

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule Stimporium.MixProject do
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
+      {:tailwind_formatter, "~> 0.4.2", only: [:dev, :test], runtime: false},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.2.0",
